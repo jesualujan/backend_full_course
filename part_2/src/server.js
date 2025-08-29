11d08bd3a4199897eba14de5b1edc55d38b1073e
jesualujan <lujan.oficial@gmail.com>
2025-08-28 21:32:15 -0600

    ✨ feat server: agregamos la ruta para cargar index.html en express

--- part_2/src/server.js
+++ part_2/src/server.js
@@ -1,18 +1,32 @@
 import express from 'express';
 import colors from 'colors';
+import path, { dirname } from 'path'; // Modulo para manejar rutas con archivos
+import { fileURLToPath } from 'url'; // Utilidad para convertir la URL del modulo en ruta de archivo
 
-const app = express();
-const PORT = process.env.PORT || 3001;
+const app = express(); // nicializamos la app con express
+const PORT = process.env.PORT || 3001; // definimos el puerto, usando una .env
+
+// obtenemos la ruta completa del archivo actual
+const __filename = fileURLToPath(import.meta.url);
+// obtenemos la ruta del archivo actual
+const __dirname = dirname(__filename);
+
+// servimos archivos estaticos desde la carpeta /public
+// esto permite que express entregue archivos como css, js, imagenes, etc.
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Middlewares
+app.use(express.json()); // parsear JSON en las solicitudes
 
 // RUTA PRINCIPAL: sirve el archivo index.html desde la carpeta /public
 app.get('/', (req, res) => {
-  res.send('bienvenido al servidor');
+  res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 // Iniciamos el servidor y mostramos un mensaje en consola
 app.listen(PORT, () => {
   console.log(
-    colors.bgMagenta(`Servidor escuchando en
-    http://localhost:${PORT}`)
+    colors.bgMagenta(`
+    🚀 Servidor escuchando en http://localhost:${PORT}`)
   );
 });
